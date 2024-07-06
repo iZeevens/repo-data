4c6706cceca9dced142ca0751328210ffa277ac1
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-06 20:24:45 +0500

    feat: add scss, remove svg files

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,7 +1,5 @@
 import { Component, ReactNode } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 interface ComponentProps {}
 
@@ -23,12 +21,8 @@ class App extends Component<ComponentProps, ComponentState> {
     return (
           <>
             <div>
-              <a href="https://vitejs.dev" target="_blank">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-              </a>
-              <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
+              <a href="https://vitejs.dev" target="_blank"></a>
+              <a href="https://react.dev" target="_blank"></a>
             </div>
             <h1>Vite + React</h1>
             <div className="card">
