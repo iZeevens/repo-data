14af12f707712655342d86b6a410eee3b85f563f
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-07 00:19:59 +0500

    feat: add states search, elements

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -8,29 +8,40 @@ interface ComponentState {}
 class App extends Component<ComponentProps, ComponentState> {
   constructor(props: ComponentProps) {
     super(props)
+    this.state = {
+      search: 'Alex',
+      elements: [],
+    }
   }
 
-  componentDidMount(): void {
-    const formData = new URLSearchParams();
-    formData.append('name', 'Alex');
+  componentDidMount = async () => {
+    const formData = new URLSearchParams()
+    formData.append('name', 'Alex')
 
-    fetch('https://stapi.co/api/v1/rest/character/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'accept': 'application/json'
-      },  
-      body: formData.toString()
-    }).then(res => res.json()).then(res => console.log(res))
+    const response = await fetch(
+      'https://stapi.co/api/v1/rest/character/search',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          accept: 'application/json',
+        },
+        body: formData.toString(),
+      }
+    )
+    const data = await response.json()
+    this.setState(() => ({ elements: data }))
   }
 
   render(): ReactNode {
     return (
       <>
-        <div className="top-section">
-          <input className='search' type="text" placeholder='Search' />
-          <button className='btn'>Search</button>
-        </div>
+        <form className="top-section">
+          <input className="search" type="text" placeholder="Search" />
+          <button className="btn" type="submit">
+            Search
+          </button>
+        </form>
         <div className="bottom-section">
           <h2>Bottom Section</h2>
         </div>
