90377e012ecb05d608fbe6887102111d33912e65
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-06 20:32:20 +0500

    feat: add 2 sections, settings json

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -3,41 +3,26 @@ import './App.scss'
 
 interface ComponentProps {}
 
-interface ComponentState {
-  count: number;
-}
+interface ComponentState {}
 
 class App extends Component<ComponentProps, ComponentState> {
   constructor(props: ComponentProps) {
-    super(props);
-    this.state = {count: 0}
+    super(props)
   }
 
-  onClickCount = () => {
-    this.setState(prevState => ({count: prevState.count + 1}))
-  }
-  
+
   render(): ReactNode {
     return (
-          <>
-            <div>
-              <a href="https://vitejs.dev" target="_blank"></a>
-              <a href="https://react.dev" target="_blank"></a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-              <button onClick={this.onClickCount}>
-                count is {this.state.count}
-              </button>
-              <p>
-                Edit <code>src/App.tsx</code> and save to test HMR
-              </p>
-            </div>
-            <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-            </p>
-          </>
-        )
+      <>
+        <div className="top-section">
+          <h1>Top Section</h1>
+        </div>
+        <div className="bottom-section">
+          <h2>Bottom Section</h2>
+          <p>This is a bigger section below.</p>
+        </div>
+      </>
+    )
   }
 }
 
