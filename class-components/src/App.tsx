9a5536c2b8c9e77a027810f7f083c2ac1659eeaa
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-06 20:21:24 +0500

    feat: migrate func component to class component

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
+import { Component, ReactNode } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface ComponentProps {}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface ComponentState {
+  count: number;
+}
+
+class App extends Component<ComponentProps, ComponentState> {
+  constructor(props: ComponentProps) {
+    super(props);
+    this.state = {count: 0}
+  }
+
+  onClickCount = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+  
+  render(): ReactNode {
+    return (
+          <>
+            <div>
+              <a href="https://vitejs.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+              </a>
+              <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+              </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+              <button onClick={this.onClickCount}>
+                count is {this.state.count}
+              </button>
+              <p>
+                Edit <code>src/App.tsx</code> and save to test HMR
+              </p>
+            </div>
+            <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+            </p>
+          </>
+        )
+  }
 }
 
 export default App
