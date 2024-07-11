f14f318de34f453eb12524b202275328ba188ff4
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-11 18:54:46 +0500

    feat: add errorBounderyButton

--- class-components/src/pages/searchPage/searchPage.tsx
+++ class-components/src/pages/searchPage/searchPage.tsx
@@ -2,22 +2,17 @@ import './searchPage.scss'
 import { useState } from 'react'
 import Search from '../../components/search/search'
 import Cards from '../../components/cards/cards'
+import ErrorBoundaryBtn from '../../components/errorBounderyButton.tsx/errorBounderyButton'
 import { Comics } from '../../interfaces/searchTypes/searchTypes'
 
 function SearchPage() {
   const [data, setData] = useState<Comics[]>()
   const [isLoading, setIsLoading] = useState(true)
 
-  const throwError = () => {
-    throw Error
-  }
-
   return (
     <>
       <div className="top-section-wrapper">
-        <button className="btn btn-error" onClick={throwError}>
-          Throw Error
-        </button>
+        <ErrorBoundaryBtn />
         <Search setDate={setData} setIsLoading={setIsLoading} />
         {data && <Cards isLoading={isLoading} elements={data} />}
       </div>
