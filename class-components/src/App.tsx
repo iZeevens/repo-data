23662f70fe4e357b4dd0f5fc2d5c96f4b678b663
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-11 22:42:35 +0500

    feat: add rounting and 404 page

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,10 +1,19 @@
-import SearchPage from './pages/searchPage/searchPage'
 import './styles/index.scss'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SearchPage from './pages/searchPage/searchPage'
+import NotFound from './pages/404/404'
 
 // Главный компонент внутри которого будут распологаться остальные компоненты
 
 function App() {
-  return <SearchPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- class-components/src/pages/404/404.tsx
+++ class-components/src/pages/404/404.tsx
@@ -1,1 +1,9 @@
 import './404.scss'
+
+function NotFound() {
+  return (
+    <span>Not Found 404</span>
+  )
+}
+
+export default NotFound
