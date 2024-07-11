4674e9aae8a6f25919bb9a12f47687729aa043e5
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-11 17:01:35 +0500

    feat: implement search component

--- class-components/src/components/search/search.tsx
+++ class-components/src/components/search/search.tsx
@@ -1,25 +1,47 @@
-import { FormEvent } from 'react'
-import { StateSearchType } from '../../interfaces/searchTypes/searchTypes'
+import { useState, FormEvent } from 'react'
+import { fetchData } from '../../services/apiService'
+// import { StateSearchType } from '../../interfaces/searchTypes/searchTypes'
 
-const searchHandler = (
-  e: FormEvent<HTMLFormElement>,
-  setState: StateSearchType,
-  fetchData: () => void
-) => {
-  e.preventDefault()
-  const target = e.target as typeof e.target & {
-    search: { value: string }
+function Search() {
+  const [searchDate, setSearchDate] = useState('')
+  const [error, setError] = useState('')
+
+  const searchHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const target = e.target as typeof e.target & {
+      search: { value: string }
+    }
+    const searchValue = target.search.value
+
+    if (
+      !/^([a-zA-Zа-яА-ЯёЁ0-9]+\s)*[a-zA-Zа-яА-ЯёЁ0-9]+$/gm.test(searchValue)
+    ) {
+      setError('No extra spaces')
+      return
+    }
+
+    localStorage.setItem('search', searchValue)
+    setSearchDate(searchValue)
+    fetchData(searchValue)
   }
-  const searchValue = target.search.value
 
-  if (!/^([a-zA-Zа-яА-ЯёЁ0-9]+\s)*[a-zA-Zа-яА-ЯёЁ0-9]+$/gm.test(searchValue)) {
-    setState({ error: 'No extra spaces' })
-    return
-  }
-
-  localStorage.setItem('search', searchValue)
-  setState({ search: searchValue })
-  fetchData()
+  return (
+    <>
+      <form className="top-section" onSubmit={searchHandler}>
+        <input
+          className="search"
+          type="text"
+          placeholder="Search"
+          name="search"
+          defaultValue={searchDate}
+        />
+        <button className="btn" type="submit">
+          Search
+        </button>
+      </form>
+      {error ? <span className="error">{error}</span> : ''}
+    </>
+  )
 }
 
-export { searchHandler }
+export { Search }
--- class-components/src/pages/searchPage/searchPage.tsx
+++ class-components/src/pages/searchPage/searchPage.tsx
@@ -1,4 +1,5 @@
 // import { fetchData } from "../../services/apiService";
+// import { searchHandler } from "../../components/search/search";
 
 function SearchPage() {}
 
