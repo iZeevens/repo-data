b59e077dbe8b56ee1b3753e4dc0c2d4df2802210
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-13 21:27:35 +0500

    feat: add fetchDataHook

---
+++ class-components/src/hooks/fetchDataHook.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import { fetchData } from '../services/apiService'
+import { Comics } from '../interfaces/searchTypes/searchTypes'
+
+function useFetchDataHook() {
+  const [query, setQuery] = useState<string | null>(null)
+  const [data, setData] = useState<Comics[]>()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [isUid, setIsUid] = useState<boolean>(false)
+
+  useEffect(() => {
+    const handleSearch = async (value: string) => {
+      setIsLoading(true)
+      const args: [string, string?] = isUid ? ['', value] : [value]
+
+      try {
+        const result = await fetchData(...args)
+        setData(result)
+      } catch {
+        console.error('Error fetch Data')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    if (query) {
+      handleSearch(query)
+    }
+  }, [query, isUid])
+
+  return { data, setData, isLoading, setIsLoading, setQuery, setIsUid }
+}
+
+export default useFetchDataHook
--- class-components/src/pages/searchPage/searchPage.tsx
+++ class-components/src/pages/searchPage/searchPage.tsx
@@ -1,33 +1,23 @@
 import './searchPage.scss'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import Search from '../../components/search/search'
 import Cards from '../../components/cards/cards'
 import Pagination from '../../components/pagination/pagination'
-import { fetchData } from '../../services/apiService'
-import { Comics } from '../../interfaces/searchTypes/searchTypes'
 import useLocalStorage from '../../hooks/localStorageHook'
+import useFetchDataHook from '../../hooks/fetchDataHook'
 import { useLocation, useNavigate, Outlet } from 'react-router-dom'
 
 function SearchPage() {
-  const [data, setData] = useState<Comics[]>()
-  const [isLoading, setIsLoading] = useState(true)
   const [searchData] = useLocalStorage('search', '')
+  const { setQuery, data, setData, isLoading, setIsLoading } = useFetchDataHook()
   const location = useLocation()
   const navigate = useNavigate()
   const pageQuery = new URLSearchParams(location.search).get('page') || '1'
   const page = parseInt(pageQuery, 10) - 1
 
   useEffect(() => {
-    const handleSearch = async (value: string) => {
-      setIsLoading(true)
-
-      const data = await fetchData(value)
-      setData(data)
-
-      setIsLoading(false)
-    }
-    handleSearch(searchData)
-  }, [searchData])
+    setQuery(searchData)
+  }, [setQuery, searchData])
 
   let dataElemsPerPage = null
   if (data) {
