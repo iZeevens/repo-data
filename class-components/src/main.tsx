ed92e0082d9c0bdb6a75676699fe805a8c388e03
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-19 19:57:46 +0500

    feat: add provider, store in main.tsx

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import ErrorBoundary from './components/errorBoundery/errorBoundery.tsx'
+import { Provider } from 'react-redux'
+import store from './redux/store.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </React.StrictMode>
+  </Provider>
 )
--- class-components/src/redux/store.tsx
+++ class-components/src/redux/store.tsx
@@ -9,7 +9,7 @@ const store = configureStore({
     getDefaultMiddleware().concat(comicsApi.middleware),
 })
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 
 export default store
