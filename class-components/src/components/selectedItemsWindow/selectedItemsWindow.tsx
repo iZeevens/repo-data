4d94dbced502c8e858e72f3f1ba4eafc67b35c90
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-07-23 00:33:16 +0500

    feat: add unselect

--- class-components/src/components/selectedItemsWindow/selectedItemsWindow.tsx
+++ class-components/src/components/selectedItemsWindow/selectedItemsWindow.tsx
@@ -1,16 +1,23 @@
 import './selectedItemsWindow.scss'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeCardDetails } from '../../redux/reducers/searchSlice'
 import { RootState } from '../../redux/store'
 
 function SelectedItemsWindow() {
-  const { cardsDetails } = useSelector(
-    (state: RootState) => state.search
-  )
+  const { cardsDetails } = useSelector((state: RootState) => state.search)
+  const dispatch = useDispatch()
+
+  const unSelectHandler = () => {
+    dispatch(removeCardDetails([]))
+  }
+
 
   return (
     <div className="selected-items-window">
-      <span className="selected-items-counter">{cardsDetails?.length} items are selected</span>
-      <button className="selected-items-unselect btn">Unselect all</button>
+      <span className="selected-items-counter">
+        {cardsDetails?.length} items are selected
+      </span>
+      <button className="selected-items-unselect btn" onClick={unSelectHandler}>Unselect all</button>
       <button className="selected-items-download btn">Download</button>
     </div>
   )
