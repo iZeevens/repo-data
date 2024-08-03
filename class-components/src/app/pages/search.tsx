013e4cb9e25b129e1a28eb10e5d58be5b761e859
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-08-03 17:38:05 +0500

    feat: search page to server component, succ running build

--- class-components/src/app/pages/search.tsx
+++ class-components/src/app/pages/search.tsx
@@ -1,34 +1,34 @@
-import { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { setPage } from '../../lib/reducers/searchSlice'
-import { useRouter } from 'next/navigation'
-import useCustomLocation from '../../hooks/navigateHook'
+// import { useEffect } from 'react'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { setPage } from '../../lib/reducers/searchSlice'
+// import { useRouter } from 'next/navigation'
+// import useCustomLocation from '../../hooks/navigateHook'
 import Search from '../../components/search/search'
 import Cards from '../../components/cards/cards'
 import Pagination from '../../components/pagination/pagination'
 import SwitchBtn from '../../components/switchTheme/switchTheme'
 import SelectedItemsWindow from '../../components/selectedItemsWindow/selectedItemsWindow'
-import { RootState } from '../../lib/store'
+// import { RootState } from '../../lib/store'
 import StoreProvider from '../StoreProvider'
 
 function SearchPage() {
-  const { data } = useSelector((state: RootState) => state.search)
-  const { page } = useCustomLocation('page')
-  const router = useRouter()
-  const dispatch = useDispatch()
+  // const { data } = useSelector((state: RootState) => state.search)
+  // const { page } = useCustomLocation('page')
+  // const router = useRouter()
+  // const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(setPage(page))
-  }, [page, dispatch])
+  // useEffect(() => {
+  //   dispatch(setPage(page))
+  // }, [page, dispatch])
 
-  useEffect(() => {
-    if (data) {
-      const dataElemsPerPage = Math.ceil(data.comics.length / 5)
-      if (page < 0 || page >= dataElemsPerPage) {
-        router.replace('/search?page=1')
-      }
-    }
-  }, [page, data, router])
+  // useEffect(() => {
+  //   if (data) {
+  //     const dataElemsPerPage = Math.ceil(data.comics.length / 5)
+  //     if (page < 0 || page >= dataElemsPerPage) {
+  //       router.replace('/search?page=1')
+  //     }
+  //   }
+  // }, [page, data, router])
 
   return (
     <StoreProvider>
