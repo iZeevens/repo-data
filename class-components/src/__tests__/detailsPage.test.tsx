c82aca401cd969f12592d357cfd2458ad30547e6
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-08-11 19:31:01 +0500

    feat: add SearchPage test

--- class-components/src/__tests__/detailsPage.test.tsx
+++ class-components/src/__tests__/detailsPage.test.tsx
@@ -1,31 +1,27 @@
 import DetailsPage from '../app/details/[detailsUid]/page'
 import { render, screen } from '@testing-library/react'
 
-const replaceMock = vi.fn()
-const getParam = vi.fn()
-
 vi.mock('next/navigation', () => ({
   useSearchParams() {
     return {
-      get: getParam,
+      get: vi.fn(),
     }
   },
   useRouter() {
     return {
-      push: replaceMock,
+      push: vi.fn(),
     }
   },
 }))
 
-
 describe('DetailsPage Testing', () => {
-  it('Check Dom', async() => {
-    render(await DetailsPage({params: { detailsUid: 'CCMA0000086083' }}))
+  it('Check Dom', async () => {
+    render(await DetailsPage({ params: { detailsUid: 'CCMA0000086083' } }))
     expect(screen.getByText(/Number Pages:/)).toBeInTheDocument()
   })
 
-  it('if data not exist', async() => {
-    render(await DetailsPage({params: { detailsUid: 'ds' }}))
+  it('if data not exist', async () => {
+    render(await DetailsPage({ params: { detailsUid: 'ds' } }))
     expect(screen.getByText(/Data not found/)).toBeInTheDocument()
   })
 })
---
+++ class-components/src/__tests__/searchPage.test.tsx
@@ -0,0 +1,24 @@
+import SearchPage from "../app/page";
+import { render, screen } from "@testing-library/react";
+
+vi.mock('next/navigation', () => ({
+  useSearchParams() {
+    return {
+      get: vi.fn(),
+    }
+  },
+  useRouter() {
+    return {
+      push: vi.fn(),
+    }
+  },
+}))
+
+
+describe('Search Page testing', () => {
+  it('In Dom', async () => {
+    render(await SearchPage({ searchParams: { search: 's', page: '1' }}))
+
+    expect(screen.getByText(/Search/)).toBeInTheDocument()
+  })
+})
