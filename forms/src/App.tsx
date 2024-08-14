b4150c03702e06008b60f897aeb1b574f79789a4
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-08-14 21:19:30 +0500

    feat: add layout, change vscode settings.json

--- forms/src/App.tsx
+++ forms/src/App.tsx
@@ -1,11 +1,14 @@
-import './App.scss'
+import './App.scss';
 
 function App() {
   return (
-    <main className='main'>
-      <h1>Hello World</h1>
+    <main className="main">
+      <section className="forms-container">
+        <span className="form first-form">First Form</span>
+        <span className="form second-form">Second Form</span>
+      </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
