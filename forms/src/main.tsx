e9837252b39a3feb29ba8be53973268ed9ce4686
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-08-15 16:51:22 +0500

    feat: add provider with store, add react-redux npm

--- forms/src/main.tsx
+++ forms/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 import App from './App.tsx';
 import UncontrolledForm from './components/uncontrolledForm/uncontrolledForm.tsx';
 import ControlledForm from './components/controlledForm/controlledForm.tsx';
@@ -10,12 +12,14 @@ import 'normalize.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path="/controlled" element={<ControlledForm />}></Route>
-        <Route path="/uncontrolled" element={<UncontrolledForm />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route path="/controlled" element={<ControlledForm />}></Route>
+          <Route path="/uncontrolled" element={<UncontrolledForm />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
