7e809c2962bc312e2ac03fdc9e4e57e33080816f
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-08-15 17:13:45 +0500

    feat: add dataFormsSlice, reducer to store

---
+++ forms/src/store/slice/dataFormsSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface FormDataState {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  gender: string;
+  acceptTerms: boolean;
+  img: File | null;
+  country: string;
+}
+
+const initialState: FormDataState = {
+  name: '',
+  age: 0,
+  email: '',
+  password: '',
+  gender: '',
+  acceptTerms: false,
+  img: null,
+  country: '',
+};
+
+const formDataSlice = createSlice({
+  name: 'formData',
+  initialState,
+  reducers: {
+    updateControlledForm(state, action: PayloadAction<FormDataState>) {
+      const { name, age, email, password, gender, acceptTerms, img, country } =
+        action.payload;
+
+      state.name = name;
+      state.age = age;
+      state.email = email;
+      state.password = password;
+      state.gender = gender;
+      state.acceptTerms = acceptTerms;
+      state.img = img;
+      state.country = country;
+    },
+  },
+});
+
+export default formDataSlice.reducer;
+export const { updateControlledForm } = formDataSlice.actions;
--- forms/src/store/store.ts
+++ forms/src/store/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import dataFormsSlice from "./slice/dataFormsSlice";
 
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: dataFormsSlice,
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
